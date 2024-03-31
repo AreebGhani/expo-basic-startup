6c6872f7dbbe8ac53568a41e2669f62ac6ff7ad2
AreebGhani <areebghanidps@gmail.com>
2024-03-31 21:56:41 +0500

    updt: Firebase Config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBa_t2NMlcnXfmDN1pq1UeZABXm0NSxwcY",
-  authDomain: "expo-basic-startup.firebaseapp.com",
-  projectId: "expo-basic-startup",
-  storageBucket: "expo-basic-startup.appspot.com",
-  messagingSenderId: "1045084862665",
-  appId: "1:1045084862665:web:da340305ccb5d6a9ca3518"
-};
+const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
